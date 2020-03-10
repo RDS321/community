e658c155dd6185ee807d2e48d9d9305013e9651f
rds <dongsheng_ran@163.com>
2020-03-11 00:58:52 +0800

    add pages

--- src/main/java/life/rds/community/community/controller/IndexController.java
+++ src/main/java/life/rds/community/community/controller/IndexController.java
@@ -1,15 +1,18 @@
 package life.rds.community.community.controller;
 
+import life.rds.community.community.dto.PaginationDTO;
 import life.rds.community.community.dto.QuestionDTO;
 import life.rds.community.community.mapper.QuestionMapper;
 import life.rds.community.community.mapper.UserMapper;
 import life.rds.community.community.model.Question;
 import life.rds.community.community.model.User;
 import life.rds.community.community.service.QuestionService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +29,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
 
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
@@ -42,8 +47,9 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page, size);
+
+        model.addAttribute("pagination", pagination);
 
         return "index";
     }
